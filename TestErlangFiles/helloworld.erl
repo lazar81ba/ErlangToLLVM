da1a90e1c5e45df6f1e9d34beb5275c94515c9fb
Bartłomiej Łazarczyk <lazar81ba@gmail.com>
2018-06-21 19:36:24 +0200

    Print variable

--- TestErlangFiles/helloworld.erl
+++ TestErlangFiles/helloworld.erl
@@ -1,4 +1,11 @@
-main()->test().
+main() ->
+      test(),
+      test(),
+      a = 1+2,
+      io:fwrite(a).
 
-test()->
-    io:fwrite(1+2*3).
+test() ->
+    io:fwrite(3+1).
+
+test2() ->
+    io:fwrite("test").
